5811232f630bd2b165726def32d72cf4eadf89ed
d2.korzun <d2.korzun@a1qa.com>
2021-01-07 02:09:31 +0300

    added elements and some actions

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DoSomething = new System.Windows.Forms.Button();
+            this.TextField = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DoSomething
+            // 
+            this.DoSomething.Location = new System.Drawing.Point(12, 41);
+            this.DoSomething.Name = "DoSomething";
+            this.DoSomething.Size = new System.Drawing.Size(75, 23);
+            this.DoSomething.TabIndex = 0;
+            this.DoSomething.Text = "Do It!";
+            this.DoSomething.UseVisualStyleBackColor = true;
+            this.DoSomething.Click += new System.EventHandler(this.DoSomething_Click);
+            // 
+            // TextField
+            // 
+            this.TextField.Location = new System.Drawing.Point(12, 12);
+            this.TextField.Name = "TextField";
+            this.TextField.Size = new System.Drawing.Size(238, 23);
+            this.TextField.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(419, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(352, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Processes:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TextField);
+            this.Controls.Add(this.DoSomething);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DoSomething;
+        private System.Windows.Forms.TextBox TextField;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private bool blink = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void DoSomething_Click(object sender, EventArgs e)
+        {
+            blink = !blink;
+            if (blink)
+            {
+                RunBlinking();
+            }
+        }
+
+        private void RunBlinking()
+        {
+            new Thread(blinkingLogic).Start();
+
+            async void blinkingLogic()
+            {
+                while (blink)
+                {
+                    ChangeTextSafe("Nice", TextField);
+                    await Task.Delay(500);
+                    ChangeTextSafe("Dick", TextField);
+                    await Task.Delay(500);
+                }
+                ChangeTextSafe("Thread finished", TextField);
+            }
+        }
+
+        private void ChangeTextSafe(string text, TextBox box)
+        {
+            if (box.InvokeRequired)
+            {
+                box.Invoke(new Action(() => ChangeTextSafe(text, box)));
+            }
+            else
+            {
+                box.Text = text;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
